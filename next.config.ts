cb15a781a1eafcd50210b64ef9ad85486002a1ef
Felipe <f7.deleon@gmail.com>
2025-08-06 13:48:01 -0300

    fix output standalone

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,9 @@ import type { NextConfig } from 'next';
 // import path from 'path';
 
 const nextConfig: NextConfig = {
-  // Recommended: this will reduce output
-  // Docker image size by 80%+
-  output: 'standalone',
-  // Optional: bring your own cache handler
-  // cacheHandler: path.resolve('./cache-handler.mjs'),
-  // cacheMaxMemorySize: 0, // Disable default in-memory caching
+  // Comment out standalone for now to get Amplify working
+  // output: 'standalone',
   images: {
-    // Optional: use a different optimization service
-    // loader: 'custom',
-    // loaderFile: './image-loader.ts',
-    //
-    // We're defaulting to optimizing images with
-    // Sharp, which is built-into `next start`
     remotePatterns: [
       {
         protocol: 'https',
@@ -25,11 +15,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // streaming responses
   compress: false,
-  // Optional: override the default (1 year) `stale-while-revalidate`
-  // header time for static pages
-  // swrDelta: 3600 // seconds
 };
 
 export default nextConfig;
